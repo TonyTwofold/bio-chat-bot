3a560aafbd05b5c5acc0788c5a80865a92cefd9f
Anton Lindegren <anton.lindegren@ericsson.com>
2018-07-12 13:57:06 +0200

    Update Verification_Token

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "GhjLWEfcU7"
+  let VERIFY_TOKEN = process.env.VERIFICATION_TOKEN
 
 
   // Parse the query params
